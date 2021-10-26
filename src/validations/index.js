300f0b3ade4a337d288a4838a31d9a8ca012755d
UyenPham09 <uyenpham09201@gmail.com>
2021-10-26 11:58:29 +0700

    first commit

---
+++ src/validations/index.js
@@ -0,0 +1,4 @@
+module.exports.authValidation = require('./auth.validation');
+module.exports.userValidation = require('./user.validation');
+module.exports.classValidation = require('./class.validation');
+module.exports.studentValidation = require('./student.validation');
